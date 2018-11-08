a02520e011967a07734317f296faa69a7f83b8c8
ChewieSolo97 <Mjdonaghy8264@eagle.fgcu.edu>
2018-11-08 17:02:59 -0500

    test

--- SpamCatcher/src/Main.java
+++ SpamCatcher/src/Main.java
@@ -1,6 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
 
   public static void main(String args[]) {
-    System.out.println("test");
+    try {
+      //Files.list(Paths.get("/Users/Mitchell/Downloads/train/")).forEach(System.out::println);
+      File dir = new File("/Users/Mitchell/Downloads/train/");
+      HashMap<String, Integer> words = new HashMap<>();
+      StringTokenizer token = new StringTokenizer(" ");
+      for (File file : dir.listFiles()) {
+        Scanner s = new Scanner(file);
+        for(int i = 0; i < 10; i++) {
+          words.put(token.nextToken(), 0);
+        }
+        s.close();
+
+      }
+    } catch(Exception e) {
+
+    }
   }
 }
